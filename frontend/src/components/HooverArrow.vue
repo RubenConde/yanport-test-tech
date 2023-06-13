78e12d6fe893da931760f863dde7a805aa9e4674
Rubén Conde Magaña <code@rubn.dev>
2023-06-13 19:44:32 +0200

    feat: Arrow added
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

---
+++ frontend/src/components/HooverArrow.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { CardinalDirections } from '@/types/CardinalDirection.enum';
+
+
+defineProps({
+  orientation: { type: String, required: true }
+})
+</script>
+
+<template>
+  <div
+    class="m-auto"
+    :class="{
+      'arrow-up': orientation === CardinalDirections.North,
+      'arrow-right': orientation === CardinalDirections.East,
+      'arrow-down': orientation === CardinalDirections.South,
+      'arrow-left': orientation === CardinalDirections.West
+    }"
+  ></div>
+</template>
+
+<style scoped>
+.arrow-up {
+  width: 0;
+  height: 0;
+  border-left: 1.5em solid transparent;
+  border-right: 1.5em solid transparent;
+  border-bottom: 1.5em solid blueviolet;
+}
+
+.arrow-down {
+  width: 0;
+  height: 0;
+  border-left: 1.5em solid transparent;
+  border-right: 1.5em solid transparent;
+  border-top: 1.5em solid blueviolet;
+}
+
+.arrow-right {
+  width: 0;
+  height: 0;
+  border-top: 1.5em solid transparent;
+  border-bottom: 1.5em solid transparent;
+  border-left: 1.5em solid blueviolet;
+}
+
+.arrow-left {
+  width: 0;
+  height: 0;
+  border-top: 1.5em solid transparent;
+  border-bottom: 1.5em solid transparent;
+  border-right: 1.5em solid blueviolet;
+}
+</style>
