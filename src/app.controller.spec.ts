7363bd879ed226d06b515f8d2bbaa5c58c176b46
Rubén Conde Magaña <code@rubn.dev>
2023-06-13 14:01:01 +0200

    feat: Test added
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CardinalDirections } from './setupHover.dto';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +16,32 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return track', () => {
+      expect(
+        appController.setupHoover({
+          gridSize: {
+            x: 10,
+            y: 10,
+          },
+          initialPosition: {
+            x: 5,
+            y: 5,
+            orientation: CardinalDirections.North,
+          },
+          commandString: 'DADADADAA',
+        }),
+      ).toStrictEqual([
+        { cmd: '-', position: { orientation: 'N', x: 5, y: 5 } },
+        { cmd: 'D', position: { orientation: 'E', x: 5, y: 5 } },
+        { cmd: 'A', position: { orientation: 'E', x: 6, y: 5 } },
+        { cmd: 'D', position: { orientation: 'S', x: 6, y: 5 } },
+        { cmd: 'A', position: { orientation: 'S', x: 6, y: 4 } },
+        { cmd: 'D', position: { orientation: 'W', x: 6, y: 4 } },
+        { cmd: 'A', position: { orientation: 'W', x: 5, y: 4 } },
+        { cmd: 'D', position: { orientation: 'N', x: 5, y: 4 } },
+        { cmd: 'A', position: { orientation: 'N', x: 5, y: 5 } },
+        { cmd: 'A', position: { orientation: 'N', x: 5, y: 6 } },
+      ]);
     });
   });
 });
