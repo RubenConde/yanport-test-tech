8c48ff4d0c3f7fbe6b7c05f08009f63f2ce601b0
Rubén Conde Magaña <code@rubn.dev>
2023-06-13 13:51:52 +0200

    feat: Update orientation method
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,35 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  private _updateOrientation(
+    currPosition: Position,
+    cmd: string,
+  ): CardinalDirections {
+    const currOrientation = currPosition.orientation;
+    const cardDirInOrder = Object.values(CardinalDirections);
+    const currOriIndex = cardDirInOrder.indexOf(currOrientation);
+    let newOrientation: CardinalDirections;
+
+    switch (cmd) {
+      case IndividualCommands.Left:
+        if (currOrientation === CardinalDirections.North)
+          newOrientation = cardDirInOrder[cardDirInOrder.length - 1];
+        else newOrientation = cardDirInOrder[currOriIndex - 1];
+        break;
+      case IndividualCommands.Right:
+        if (currOrientation === CardinalDirections.West)
+          newOrientation = cardDirInOrder[0];
+        else newOrientation = cardDirInOrder[currOriIndex + 1];
+        break;
+
+      default:
+        return newOrientation;
+        break;
+    }
+    return newOrientation;
+  }
+
   private _updatePosition(currPosition: Position, gridSize: Coordinates) {
     const newCoordinates: Coordinates = {
       x: currPosition.x,
