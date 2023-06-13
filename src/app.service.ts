b7308754057525496dafb3b440570a098d761786
Rubén Conde Magaña <code@rubn.dev>
2023-06-13 13:51:31 +0200

    feat: Update position method
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,28 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private _updatePosition(currPosition: Position, gridSize: Coordinates) {
+    const newCoordinates: Coordinates = {
+      x: currPosition.x,
+      y: currPosition.y,
+    };
+    switch (currPosition.orientation) {
+      case CardinalDirections.North:
+        if (currPosition.y < gridSize.y) newCoordinates.y++;
+        break;
+      case CardinalDirections.East:
+        if (currPosition.x < gridSize.x) newCoordinates.x++;
+        break;
+      case CardinalDirections.South:
+        if (currPosition.y > 0) newCoordinates.y--;
+        break;
+      case CardinalDirections.West:
+        if (currPosition.x > 0) newCoordinates.x--;
+        break;
+      default:
+        return newCoordinates;
+        break;
+    }
+    return newCoordinates;
   }
 }
